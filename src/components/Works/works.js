9d1fea14f01481e18e2ca965d6f6af6fa402cdc2
Nishit Mistry <nishitmistry@wirelessprv-10-193-26-77.near.illinois.edu>
2025-09-22 14:26:17 -0500

    Fix CI build: remove unused imports in Works

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,8 +1,6 @@
 import React from "react";
 import './works.css';
 import WorkEx1 from '../../assets/WE1.png';
-import WorkEx2 from '../../assets/WE2.png';
-import WorkEx3 from '../../assets/WE3.png';
 import WorkEx4 from '../../assets/WE4.png';
 import WorkEx5 from '../../assets/WE5.png';
 import WorkEx6 from '../../assets/WE6.png';
